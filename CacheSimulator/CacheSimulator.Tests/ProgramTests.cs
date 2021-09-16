58a8f7da79f1fdc01a0800f9c3dd33c7ae095f97
Offek Hemo <snipo7707@gmail.com>
2021-09-16 08:12:50 +0300

    prepare a "tests-runner"

--- CacheSimulator/CacheSimulator.Tests/ProgramTests.cs
+++ CacheSimulator/CacheSimulator.Tests/ProgramTests.cs
@@ -1,4 +1,7 @@
 ﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace CacheSimulator.Tests
 {
@@ -15,14 +18,43 @@ namespace CacheSimulator.Tests
         public void ProgramTests1()
         {
             //arrange
-            
+            var prog = new Program();
+            //string[] files = Directory.GetFiles("path");
+            string[] args = prepareArgs("file");
+
 
             //act
-           
+
             //assert
-            
 
 
+
+
+
+
+        }
+        public string[] prepareArgs(string filePath)
+        {
+            List<string> arguments = null;
+            using (var file = File.OpenText(filePath))
+            {
+                string content;
+                if ((content = file.ReadLine()) != null)
+                {
+                    arguments = content.Split(' ').ToList();
+                }
+
+            }
+
+            if(arguments!=null)
+            {
+                Assert.AreEqual(arguments[0].ToCharArray()[0], ".");
+                Assert.AreEqual(arguments[0].ToCharArray()[1], "/");
+                arguments.Remove(arguments[0]);
+                return arguments.ToArray();
+            }
+
+            return null;
         }
     }
 }
